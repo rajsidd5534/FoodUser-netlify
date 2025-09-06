e179607b2e6d6b602d2c5176522426cbbc874299
Md Raj Siddiqui <rajsidd5534@gmail.com>
2025-09-07 04:24:55 +0530

    update url

--- src/Service/orderService.js
+++ src/Service/orderService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/orders";
+const API_URL = "https://fooapi-backend-production.up.railway.app/api/orders";
 
 export const fetchUserOrders = async (token) => {
     try {
